fac48f68bb09e2bfdc19a56c5ab3acd2af9e891f
bootforge-dev <themadhavp94@gmail.com>
2026-09-23 23:44:48 +0530

    added openapi config to gateway server

--- inventory-service/src/main/java/com/bootforge/inventory/config/OpenApiConfig.java
+++ inventory-service/src/main/java/com/bootforge/inventory/config/OpenApiConfig.java
@@ -3,15 +3,25 @@ package com.bootforge.inventory.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${app.api-gateway-url:http://localhost:9100}")
+    private String apiGatewayUrl;
+
     @Bean
     public OpenAPI inventoryServiceOpenAPI() {
         return new OpenAPI()
+                .servers(java.util.List.of(
+                        new Server()
+                                .url(apiGatewayUrl)
+                                .description("API Gateway")
+                ))
                 .info(new Info()
                         .title("Inventory Service API")
                         .description("REST APIs for managing inventories")
@@ -19,5 +29,4 @@ public class OpenApiConfig {
                         .contact(new Contact()
                                 .name("BootForge")));
     }
-
 }
--- product-service/src/main/java/com/bootforge/productservice/config/OpenApiConfig.java
+++ product-service/src/main/java/com/bootforge/productservice/config/OpenApiConfig.java
@@ -3,15 +3,26 @@ package com.bootforge.productservice.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${app.api-gateway-url:http://localhost:9100}")
+    private String apiGatewayUrl;
+
     @Bean
     public OpenAPI productServiceOpenAPI() {
+
         return new OpenAPI()
+                .servers(java.util.List.of(
+                        new Server()
+                                .url(apiGatewayUrl)
+                                .description("API Gateway")
+                ))
                 .info(new Info()
                         .title("Product Service API")
                         .description("REST APIs for managing products")
@@ -19,5 +30,4 @@ public class OpenApiConfig {
                         .contact(new Contact()
                                 .name("BootForge")));
     }
-
 }
